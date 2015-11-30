56d0a929122c759aa2253d94db1b133f1f7c1003
wds <wds@it-step.edu>
2015-12-01 00:50:04 +0200

    Class Math Method Add

--- TestConsoleApplication/TestConsoleApplication/Program.cs
+++ TestConsoleApplication/TestConsoleApplication/Program.cs
@@ -5,6 +5,14 @@ using System.Text;
 
 namespace TestConsoleApplication
 {
+    class Math
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
